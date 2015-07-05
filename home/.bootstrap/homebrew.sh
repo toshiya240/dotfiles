77583683a041e61dfccbbe68a47ad38885cc5a92
Toshiya NISHIO <toshiya240@gmail.com>
2015-07-05 09:28:32 +0900

    Replace brew bundle with brew-file because brew bundle is deprecated

--- home/.bootstrap/homebrew.sh
+++ home/.bootstrap/homebrew.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-cd $HOME
-sudo brew bundle
+brew install rcmdnk/file/brew-file
+brew file set_repo toshiya240/Brewfile
+brew file install
