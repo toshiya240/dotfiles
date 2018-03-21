ede5020a7ca7f6cb79e06363310266474cd1d337
Toshiya NISHIO <toshiya240@gmail.com>
2018-03-21 10:06:39 +0900

    [bootstrap] update url of homebrew

--- home/.bootstrap/homebrew.sh
+++ home/.bootstrap/homebrew.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install rcmdnk/file/brew-file
 brew file set_repo toshiya240/Brewfile
 brew file install
