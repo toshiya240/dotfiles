5c494832f71d5e762dc5e6efb0e27b5bfcc7527a
Toshiya NISHIO <toshiya240@gmail.com>
2018-03-22 08:42:45 +0900

    [fish] set the less input preprocessor

--- home/.config/fish/conf.d/profile.fish
+++ home/.config/fish/conf.d/profile.fish
@@ -35,6 +35,13 @@ if status is-login >/dev/null; and test -z "$TMUX"
     # Remove -X and -F (exit if the content fits on one screen) to enable it.
     set -x LESS '-F -g -i -M -R -S -w -X -z-4'
 
+    # Set the Less input preprocessor
+    if type -q src-hilite-lesspipe.sh
+        set -x LESSOPEN '| src-hilite-lesspipe.sh %s'
+    else if type -q lesspipe.sh
+        set -x LESSOPEN '| lesspipe.sh %s'
+    end
+
     #
     # Temporary Files
     #
