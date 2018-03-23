6e6aa707fd529e574b0fb6493fad7c7a656d0752
Toshiya NISHIO <toshiya240@gmail.com>
2018-03-23 11:20:34 +0900

    remove bootstrap script

--- home/.bootstrap/homebrew.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install rcmdnk/file/brew-file
-brew file set_repo toshiya240/Brewfile
-brew file install
